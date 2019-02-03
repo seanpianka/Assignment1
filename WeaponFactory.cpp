97c8ece2be61bad163bb97071179e737157e1402
Sean Pianka <pianka@eml.cc>
2019-02-03 15:31:10 -0500

    Update WeaponFactory and main function/testcases
    Add headers for new weapon classes to allow for factory to instantiate
    the weapons upon user request.
    Update main function's testcases for testing new weapons.

--- WeaponFactory.cpp
+++ WeaponFactory.cpp
@@ -1,31 +1,47 @@
-/* 
+/*
  * File:   WeaponFactory.cpp
  * Author: Javier <jrescobara@gmail.com>
- *  
- */ 
+ *
+ */
 
 #include <string>
-#include <stddef.h> 
+#include <stddef.h>
 #include "WeaponFactory.h"
-#include "CommonSword.h" 
+#include "CommonSword.h"
 #include "CommonSpear.h"
+#include "SimpleAxe.h"
+#include "SimpleHammer.h"
+#include "CrazyRandomSword.h"
+
 
 WeaponFactory* WeaponFactory::instance = NULL;
 
-WeaponFactory * WeaponFactory::getInstance() {
+WeaponFactory* WeaponFactory::getInstance() {
     if (!instance) {
         instance = new WeaponFactory();
     }
     return instance;
 }
 
-Weapon * WeaponFactory::getWeapon(std::string name) {
+Weapon* WeaponFactory::getWeapon(std::string name) {
     if (name.compare("sword") == 0) {
         return new CommonSword();
     }
-    
+
     if (name.compare("spear") == 0) {
         return new CommonSpear();
+    }
+
+    if (name.compare("hammer") == 0) {
+        return new SimpleHammer();
+    }
+
+    if (name.compare("axe") == 0) {
+        return new SimpleAxe();
+    }
+
+    if (name.compare("crazy random sword") == 0) {
+        return new CrazyRandomSword();
     }
 
     throw "Invalid weapon";
